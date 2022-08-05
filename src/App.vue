224bd9bf3b7827b20b53f048f75c6514bff160ba
rajbhatt <rjbhatt110@gmail.com>
2022-08-06 01:18:37 +0530

    field update

--- src/App.vue
+++ src/App.vue
@@ -258,6 +258,7 @@
                       name="subject"
                       id="subject"
                       autocomplete="given-name"
+                      value="Autotag node"
                       class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                     />
                   </div>
